aeeee496bac7062db00fa882e74b0cdd790e29c4
pradip dharam <pradip.dharam@gmail.com>
2024-05-13 03:00:43 +0530

    Corrected the read_card method's of ReadyState & CardReadingState.

--- state/card_reading_state.py
+++ state/card_reading_state.py
@@ -1,6 +1,8 @@
 from atm import ATM
+from card_manager import CardManagerFactory
 from data import ATMState, CardDetails
-from state import State, CardEjectingState, ReadyState
+from db import DBAccessor
+from state import State, CardEjectingState, ReadyState, StateFactory
 
 
 class CardReadingState(State):
@@ -10,13 +12,25 @@ class CardReadingState(State):
         self.__atm = atm
 
     def init(self):
-        self.__atm.change_state(ReadyState(self.__atm))
+        raise ValueError("Invalid operation. Illegal state exception")
 
     def cancel_transaction(self, transaction_id: int) -> bool:
         raise ValueError("Invalid operation. Illegal state exception")
 
     def read_card(self, card_details: CardDetails) -> None:
-        pass
+        valid_card = (CardManagerFactory
+                      .get_card_manager(CardDetails.card_type)
+                      .validate_card(card_details))
+        DBAccessor.persist_card_details(card_details, self.__atm.machine_id)
+        if valid_card:
+            state_withdraw_details_reading = (
+                StateFactory.get_state(ATMState.WITHDRAWAL_DETAILS_READING, self.__atm))
+            self.__atm.change_state(state_withdraw_details_reading)
+        else:
+            DBAccessor.disapprove_transaction(self.__atm.machine_id)
+            # ENUM would be needed for transaction status, let's define.
+            state_ready = StateFactory.get_state(ATMState.READY, self.__atm)
+            self.__atm.change_state(state_ready)
 
     def read_withdrawal_details(self, card_type: str,
                                 card_num: int, pin: int):
--- state/ready_state.py
+++ state/ready_state.py
@@ -20,19 +20,7 @@ class ReadyState(State):
         return transaction_id
 
     def read_card(self, card_details: CardDetails) -> None:
-        valid_card = (CardManagerFactory
-                      .get_card_manager(CardDetails.card_type)
-                      .validate_card(card_details))
-        DBAccessor.persist_card_details(card_details, self.__atm.machine_id)
-        if valid_card:
-            state_withdraw_details_reading = (
-                StateFactory.get_state(ATMState.WITHDRAWAL_DETAILS_READING, self.__atm))
-            self.__atm.change_state(state_withdraw_details_reading)
-        else:
-            DBAccessor.disapprove_transaction(self.__atm.machine_id)
-            # ENUM would be needed for transaction status, let's define.
-            state_ready = StateFactory.get_state(ATMState.READY, self.__atm)
-            self.__atm.change_state(state_ready)
+        raise ValueError("Invalid operation. Illegal state exception")
 
     def cancel_transaction(self, transaction_id: str) -> bool:
         DBAccessor.cancel_transaction(transaction_id)
@@ -41,7 +29,7 @@ class ReadyState(State):
         return True
 
     def read_withdrawal_details(self, card_type: str, card_num: int, pin: int):
-        raise ValueError("Invalid operation")
+        raise ValueError("Invalid operation. Illegal state exception")
 
     def dispense_cash(self, transaction_id: int) -> bool:
         raise ValueError("Invalid operation. Illegal state exception")
